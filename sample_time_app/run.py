6dfded91fb74429eebba5376d82e67a4f4cd9642
Bradley Cushing <bradleycushing@gmail.com>
2024-02-10 16:22:27 -0500

    feat(time): add time route for displaying current time

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,12 +1,17 @@
 from flask import Flask
+from datetime import datetime
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return 'Hello world!'
+    return "Hello world!"
 
 
-app.run(host='0.0.0.0',
-        port=8080,
-        debug=True)
+@app.route("/time")
+def time():
+    return str(datetime.now())
+
+
+app.run(host="0.0.0.0", port=8080, debug=True)
